6811e3860ae1117970558e75eb0c2552892ea0d1
zhengyiming <302837029@qq.com>
2020-08-16 21:39:48 +0800

    企业微信po封装

--- api/util.py
+++ api/util.py
@@ -3,8 +3,8 @@ import requests
 class Util:
     def get_token(self):
         request_parma={
-            "corpid" : "wwb24e6fe4d7d1c71a",
-            "corpsecret" : "yIhI9TMNunyO38XkFESwsKUb1tfcBWtw01LZmOz7Xvc"
+            "corpid" : "xxxxxxxx",
+            "corpsecret" : "xxxxxxxx"
         }
         r=requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken",params=request_parma)
         return r.json()['access_token']
--- test_demo.py
+++ test_demo.py
@@ -16,8 +16,8 @@ class TestDemo:
     @pytest.fixture(scope="session")
     def token(self):
         request_parma={
-            "corpid" : "wwb24e6fe4d7d1c71a",
-            "corpsecret" : "yIhI9TMNunyO38XkFESwsKUb1tfcBWtw01LZmOz7Xvc"
+            "corpid" : "xxxxxxxx",
+            "corpsecret" : "xxxxxxxx"
         }
         r=requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken",params=request_parma)
         return r.json()['access_token']
